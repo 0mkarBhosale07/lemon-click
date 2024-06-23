40d6df640620a66e13728190686df1b1a4f3ff1d
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2024-06-23 19:40:05 +0530

    Stats for discord bot

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -1,8 +1,10 @@
+import { connectToDatabase } from "@/db";
 import Links from "@/models/links.model";
 import Users from "@/models/users.model";
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
 export async function GET() {
+  connectToDatabase();
   const accounts = await Users.countDocuments();
   const links = await Links.countDocuments();
   return Response.json({ accounts: accounts, links: links }); // This is a test endpoint for now
