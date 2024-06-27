9b372b2f00f72642298d15f71b3b6213e7430c02
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2024-06-27 21:41:21 +0530

    Previous Redirection Code

--- src/app/redirect/[id]/page.tsx
+++ src/app/redirect/[id]/page.tsx
@@ -2,10 +2,11 @@
 import React, { useState, useEffect } from "react";
 import { getLink } from "@/actions/links.action";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const RedirectionPage = ({ params }: { params: { id: string } }) => {
   const { id }: any = params;
-  const [link, setLink] = useState<string | null>(null);
+  const [link, setLink] = useState<string | null>(null); // Set initial state to null
   const [resData, setResData] = useState<any>(null);
 
   useEffect(() => {
@@ -17,18 +18,32 @@ const RedirectionPage = ({ params }: { params: { id: string } }) => {
         setResData(res);
         setLink(res.originalUrl);
 
-        // Open the link in a new tab after 1 second
-        setTimeout(() => {
-          if (res.originalUrl) {
-            window.open(res.originalUrl, "_blank");
-          }
-        }, 1000);
+        if (res.androidIntentUrl) {
+          console.log("Redirecting to Android intent URL");
+          window.location.href = res.androidIntentUrl;
+          setTimeout(() => {
+            window.location.href = res.fallbackUrl;
+          }, 1000);
+        } else if (res.iosUrl) {
+          console.log("Redirecting to iOS URL");
+          window.location.href = res.iosUrl; // ios issue!
+          setTimeout(() => {
+            window.location.href = res.fallbackUrl;
+          }, 1000);
+        } else if (res.defaultBrowserUrl) {
+          console.log("Opening in default browser");
+          window.open(res.defaultBrowserUrl, "_blank"); // Open in new tab
+        } else {
+          console.log("Unsupported URL or no mapping found");
+          // Handle case where no mapping or default URL is available
+        }
       } catch (error) {
         console.error("Error fetching link:", error);
+        // Handle error accordingly, maybe set an error state
       }
     };
     getData();
-  }, [id]);
+  }, []);
 
   return (
     <div className="mt-5">
@@ -37,13 +52,13 @@ const RedirectionPage = ({ params }: { params: { id: string } }) => {
       </h1>
       <div className="btn flex justify-center mt-10">
         {link ? (
-          // <a href={link} target="_blank" rel="noopener noreferrer">
-          // </a>
-          <Button>Redirect</Button>
+          <Link href={link} target="_blank" rel="noopener noreferrer">
+            <Button>Redirect</Button>
+          </Link>
         ) : (
           <div className="flex justify-center items-center h-24">
             <div className="w-8 h-8 border-4 border-t-4 border-t-blue-500 border-blue-200 rounded-full animate-spin"></div>
-          </div>
+          </div> // Show loading state or a message
         )}
       </div>
     </div>
